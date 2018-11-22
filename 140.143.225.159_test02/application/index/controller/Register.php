aa5e45d66c6d2c8fff25efa1e0ad22ca377d8b3f
Jason <rishins@qq.com>
2018-11-22 10:19:16 +0800

    init conmmit

---
+++ 140.143.225.159_test02/application/index/controller/Register.php
@@ -0,0 +1,23 @@
+<?php
+namespace app\index\controller; 
+
+use think\Controller; 
+class Register extends Controller {
+	public function index() {
+		return $this -> fetch(); 
+    }
+    public function direct(){
+        $this->redirect(url('register/index'));
+    }
+    public function doRegister(){
+		$param = input('post.');
+		$sqlInsertCommand = "insert into users(`user_name`,`user_pwd`) value('".$param['user_name']."'".","."'".$param['user_pwd']."');";
+		$data=db('users')->execute($sqlInsertCommand);
+		//$data=db('users')->execute("insert into users(`user_name`,`user_pwd`) value('aa','aa');");
+		$this->redirect(url('register/registOK'));
+	}
+	public function registOK(){
+		// TODO 获取注册的用户名？
+		echo "注册成功!" . ' <a href="' . url('login/loginout') . '">退出</a>'; 
+	}
+}
