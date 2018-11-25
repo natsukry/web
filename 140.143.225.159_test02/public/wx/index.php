5c68a4dc33bae5bb7fe4be21613542a1783eb46e
Jason <rishins@qq.com>
2018-11-25 17:52:08 +0800

    write README

--- 140.143.225.159_test02/public/wx/index.php
+++ 140.143.225.159_test02/public/wx/index.php
@@ -16,77 +16,40 @@
         echo  $echostr;
         exit;
     }
-//1.获取到微信推送过来post数据（xml格式）
-        $postArr = $GLOBALS['HTTP_RAW_POST_DATA'];
-        //2.处理消息类型，并设置回复类型和内容
-        /*<xml>
-<ToUserName><![CDATA[toUser]]></ToUserName>
-<FromUserName><![CDATA[FromUser]]></FromUserName>
-<CreateTime>123456789</CreateTime>
-<MsgType><![CDATA[event]]></MsgType>
-<Event><![CDATA[subscribe]]></Event>
-</xml>*/
-        $postObj = simplexml_load_string($postArr);
-        //$postObj->ToUserName = '';
-        //$postObj->FromUserName = '';
-        //$postObj->CreateTime = '';
-        //$postObj->MsgType = '';
-        //$postObj->Event = '';
-        // gh_e79a177814ed
-        //判断该数据包是否是订阅的事件推送
-        if (strtolower($postObj->MsgType) == 'event') {
-            //如果是关注 subscribe 事件
-            if (strtolower($postObj->Event == 'subscribe')) {
-                //回复用户消息(纯文本格式)
-                $toUser   = $postObj->FromUserName;
-                $fromUser = $postObj->ToUserName;
-                $time     = time();
-                $msgType  =  'text';
-                $content  = '欢迎关注我们的微信公众账号'.$postObj->FromUserName.'-'.$postObj->ToUserName;
-                $template = "<xml>
-                                <ToUserName><![CDATA[%s]]></ToUserName>
-                                <FromUserName><![CDATA[%s]]></FromUserName>
-                                <CreateTime>%s</CreateTime>
-                                <MsgType><![CDATA[%s]]></MsgType>
-                                <Content><![CDATA[%s]]></Content>
-                                </xml>";
-                $info     = sprintf($template, $toUser, $fromUser, $time, $msgType, $content);
-                echo $info;
-                /*<xml>
-                <ToUserName><![CDATA[toUser]]></ToUserName>
-                <FromUserName><![CDATA[fromUser]]></FromUserName>
-                <CreateTime>12345678</CreateTime>
-                <MsgType><![CDATA[text]]></MsgType>
-                <Content><![CDATA[你好]]></Content>
-                </xml>*/
-            }
-        }
-
-        // 获取用户发送的消息
-        if (strtolower($postObj->MsgType) == 'text') {
+    //1.获取到微信推送过来post数据（xml格式）
+    $postArr = $GLOBALS['HTTP_RAW_POST_DATA'];
+    //2.处理消息类型，并设置回复类型和内容
+    /*<xml>
+    <ToUserName><![CDATA[toUser]]></ToUserName>
+    <FromUserName><![CDATA[FromUser]]></FromUserName>
+    <CreateTime>123456789</CreateTime>
+    <MsgType><![CDATA[event]]></MsgType>
+    <Event><![CDATA[subscribe]]></Event>
+    </xml>*/
+    $postObj = simplexml_load_string($postArr);
+    //$postObj->ToUserName = '';
+    //$postObj->FromUserName = '';
+    //$postObj->CreateTime = '';
+    //$postObj->MsgType = '';
+    //$postObj->Event = '';
+    // gh_e79a177814ed
+    //判断该数据包是否是订阅的事件推送
+    if (strtolower($postObj->MsgType) == 'event') {
+        //如果是关注 subscribe 事件
+        if (strtolower($postObj->Event == 'subscribe')) {
             //回复用户消息(纯文本格式)
-            $userText = $postObj->Content;
             $toUser   = $postObj->FromUserName;
             $fromUser = $postObj->ToUserName;
             $time     = time();
             $msgType  =  'text';
-
-            $textSendToUser = $userText; // 默认回复消息为用户发送的消息
-            if($userText == '天气' || $userText == '北京天气') {
-                $textSendToUser = "晴，PM2.5 30 空气质量优，东南风，湿度30%，温度20~23摄氏度";
-            }
-            if($userText == '网页' || $userText == '网页天气' || '网页北京天气') {
-                $textSendToUser = "http://140.143.225.159/weather.html";
-            }
-
-            $content  = $textSendToUser;
+            $content  = '欢迎关注我们的微信公众账号'.$postObj->FromUserName.'-'.$postObj->ToUserName;
             $template = "<xml>
-                                <ToUserName><![CDATA[%s]]></ToUserName>
-                                <FromUserName><![CDATA[%s]]></FromUserName>
-                                <CreateTime>%s</CreateTime>
-                                <MsgType><![CDATA[%s]]></MsgType>
-                                <Content><![CDATA[%s]]></Content>
-                                </xml>";
+                            <ToUserName><![CDATA[%s]]></ToUserName>
+                            <FromUserName><![CDATA[%s]]></FromUserName>
+                            <CreateTime>%s</CreateTime>
+                            <MsgType><![CDATA[%s]]></MsgType>
+                            <Content><![CDATA[%s]]></Content>
+                            </xml>";
             $info     = sprintf($template, $toUser, $fromUser, $time, $msgType, $content);
             echo $info;
             /*<xml>
@@ -97,3 +60,39 @@
             <Content><![CDATA[你好]]></Content>
             </xml>*/
         }
+    }
+
+    // 获取用户发送的消息
+    if (strtolower($postObj->MsgType) == 'text') {
+        //回复用户消息(纯文本格式)
+        $userText = $postObj->Content;
+        $toUser   = $postObj->FromUserName;
+        $fromUser = $postObj->ToUserName;
+        $time     = time();
+        $msgType  =  'text';
+
+        $textSendToUser = $userText; // 默认回复消息为用户发送的消息
+        if ($userText == '天气' || $userText == '北京天气') {
+            $textSendToUser = "晴，PM2.5 30 空气质量优，东南风，湿度30%，温度20~23摄氏度";
+        } elseif ($userText == '网页' || $userText == '网页天气' || '网页北京天气') {
+            $textSendToUser = "http://140.143.225.159/weather.html";
+        }
+
+        $content  = $textSendToUser;
+        $template = "<xml>
+                            <ToUserName><![CDATA[%s]]></ToUserName>
+                            <FromUserName><![CDATA[%s]]></FromUserName>
+                            <CreateTime>%s</CreateTime>
+                            <MsgType><![CDATA[%s]]></MsgType>
+                            <Content><![CDATA[%s]]></Content>
+                            </xml>";
+        $info     = sprintf($template, $toUser, $fromUser, $time, $msgType, $content);
+        echo $info;
+        /*<xml>
+        <ToUserName><![CDATA[toUser]]></ToUserName>
+        <FromUserName><![CDATA[fromUser]]></FromUserName>
+        <CreateTime>12345678</CreateTime>
+        <MsgType><![CDATA[text]]></MsgType>
+        <Content><![CDATA[你好]]></Content>
+        </xml>*/
+    }
